fcbb0982d660267baa74edede6e26ed9b14a88b0
abbasyazdanmehr <kjkj55047@gmail.com>
2024-08-01 11:41:42 +0330

    feat: add device and log api views

---
+++ server-app/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework import routers
+from . import views
+
+router = routers.DefaultRouter()
+router.register('devices', views.DeviceView, )
+router.register('logs', views.LogView, )
+
+
+urlpatterns = [
+    path('v1/', include(router.urls)),
+]
--- server-app/api/views.py
+++ server-app/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .models import Device, Log
+from .serializers import DeviceSerializer, LogSerializer
+
+class DeviceView(viewsets.ModelViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+class LogView(viewsets.ModelViewSet):
+    queryset = Log.objects.all()
+    serializer_class = LogSerializer
+    
