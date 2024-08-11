4cec18fc4812777181b18b2c69cbf339b54607ae
abbasyazdanmehr <kjkj55047@gmail.com>
2024-08-11 17:46:33 +0330

    fix: add static file urls

--- server-app/config/urls.py
+++ server-app/config/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from notification.consumer import NotificationConsumer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('notification/', include('notification.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 websocket_urlpatterns = [
     path("ws/", NotificationConsumer.as_asgi())
