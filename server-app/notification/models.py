66d202022cc29a25498526d90dba2042aa3bb16f
abbasyazdanmehr <kjkj55047@gmail.com>
2024-08-27 18:01:16 +0330

    fix: status field not-null temporary fix

--- server-app/notification/models.py
+++ server-app/notification/models.py
@@ -5,6 +5,7 @@ class Notification(models.Model):
     message = models.TextField()
     target_user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, null=True)
 
     def __str__(self) -> str:
         return "{ Notification: message: " + self.message + " }"
